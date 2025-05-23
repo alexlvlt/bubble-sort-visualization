aa8c2ebb8d92eed0b6ca8d8a94c61093b10a0e9b
Alexandra <acabova.khalilova@gmail.com>
2025-05-23 12:40:08 +0300

    add bubble sort algorithm

---
+++ src/lib/bubble-sort.ts
@@ -0,0 +1,34 @@
+import { ArrayValuesStates } from "../types/types.ts";
+
+interface SortItem {
+  value: number;
+  state: ArrayValuesStates;
+}
+
+export function* bubbleSort(array: SortItem[]): Generator<SortItem[], void, unknown> {
+  const copyArr = [...array];
+  let swapped = false;
+  let counter = 0;
+
+  do {
+    swapped = false;
+    counter++;
+
+    for (let i = 0; i < copyArr.length - counter; i++) {
+      copyArr[i].state = ArrayValuesStates.Checked;
+      copyArr[i + 1].state = ArrayValuesStates.Checked;
+      yield [...copyArr];
+
+      if (copyArr[i].value > copyArr[i + 1].value) {
+        swapped = true;
+        copyArr[i].state = ArrayValuesStates.First;
+        copyArr[i + 1].state = ArrayValuesStates.Second;
+        [copyArr[i], copyArr[i + 1]] = [copyArr[i + 1], copyArr[i]];
+      }
+
+      yield [...copyArr];
+      copyArr[i].state = ArrayValuesStates.Off;
+      copyArr[i + 1].state = ArrayValuesStates.Off;
+    }
+  } while (swapped);
+}
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export enum ArrayValuesStates {
+  First = "first",
+  Second = "second",
+  Checked = "checked",
+  Off = "off",
+}
