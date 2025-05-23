7dfb05f2a8941403b46cc007c04af285427ffa9e
Alexandra <acabova.khalilova@gmail.com>
2025-05-23 15:29:41 +0300

    add ItemsList component

---
+++ src/components/ItemsList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="items-list">
+    <Slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less">
+.items-list {
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+}
+</style>
